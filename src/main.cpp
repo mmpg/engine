1a9fc8c7f366c7dff00df141944502956b96b383
Héctor Ramón <hector0193@gmail.com>
2015-11-01 02:12:35 +0100

    Reply test requests

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,26 @@
 #include <iostream>
+#include <zmq.hpp>
 
-using namespace std;
+int main ()  {
+  // Prepare our context and socket
+  zmq::context_t context(1);
+  zmq::socket_t socket(context, ZMQ_REP);
+  socket.bind("tcp://*:5555");
 
-int main() {
-  cout << "Hello, World!" << endl;
+  std::cout << "Listening to 0.0.0.0:5555" << std::endl;
+
+  while(true) {
+    zmq::message_t request;
+
+    // Wait for next request from client
+    socket.recv(&request);
+    std::cout << "Received request" << std::endl;
+
+    // Send reply back to client
+    zmq::message_t reply(8);
+    memcpy((void*) reply.data(), "It works", 8);
+    socket.send(reply);
+  }
+
   return 0;
 }
