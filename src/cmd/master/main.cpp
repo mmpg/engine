18a78999575bf46026226fc406dc2c50e87d2dcf
Héctor Ramón <hector0193@gmail.com>
2015-11-09 03:24:35 +0100

    Remove unnecessary master class

--- src/cmd/master/main.cpp
+++ src/cmd/master/main.cpp
@@ -1,14 +1,31 @@
-#include "../../utils.hpp"
 #include "../../debug.hpp"
-#include "player.hpp"
-#include "master.hpp"
+#include "worker.hpp"
+#include <zmq.hpp>
 
 using namespace mmpg;
 
-mmpg::Master master;
+mmpg::Worker worker;
 
 int main() {
-  master.Run();
+  // Start game world server
+  zmq::context_t zcontext(1);
+
+  zmq::socket_t server(zcontext, ZMQ_REP);
+  server.bind("tcp://*:5557");
+
+  worker.Run();
+
+  debug::Println("MASTER", "Listening to 0.0.0.0:5577");
+
+  while(true) {
+    zmq::message_t request;
+    server.recv(&request);
+
+    std::string data = std::string(static_cast<char*>(request.data()), request.size());
+
+    debug::Println("MASTER", "    Request received: " + data);
+    server.send("reply", 5); // TODO: Send game world status
+  }
 
   return 0;
 }
