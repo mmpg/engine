a4516ba99514dc33af684c836fcdd0c46a9adf75
Héctor Ramón <hector0193@gmail.com>
2015-11-21 14:25:48 +0100

    Write log of actions

--- src/log.cpp
+++ src/log.cpp
@@ -1,19 +1,51 @@
 #include "log.hpp"
+#include "utils.hpp"
 
 namespace mmpg {
 
-Log::Log(std::string directory) : directory_(directory) {
+Log::Log(std::string directory) : directory_(directory), file_(0) {
+  utils::Mkdir(directory, 0775);
 
+  new_log_file(current_interval());
 }
 
-void Log::Add(std::string line) {
+void Log::Add(const std::string& line) {
   lines_.push_back(line);
 }
 
 
 void Log::Flush() {
-  // TODO: Write log file
+  if(lines_.size() > 0) {
+    *file_ << lines_[0];
+  }
+
+  for(int i = 1; i < lines_.size(); ++i) {
+    *file_ << '\n' << lines_[i];
+  }
+
+  *file_ << std::endl;
+
   lines_.clear();
+
+  std::time_t current = current_interval();
+
+  if(interval_ != current) {
+    new_log_file(current);
+  }
 }
 
+std::time_t Log::current_interval() const {
+  return (std::time(0) / 300) * 300;
+}
+
+void Log::new_log_file(const std::time_t& current) {
+  interval_ = current;
+
+  if(file_) {
+    file_->close();
+    delete file_;
+  }
+
+  file_ = new std::ofstream(directory_ + "/" + std::to_string(interval_) + ".log");
+}
 }
--- src/log.hpp
+++ src/log.hpp
@@ -2,6 +2,8 @@
 
 #include <string>
 #include <vector>
+#include <fstream>
+#include <ctime>
 
 namespace mmpg {
 
@@ -9,12 +11,17 @@ class Log {
  public:
   Log(std::string directory);
 
-  void Add(std::string line);
+  void Add(const std::string& line);
   void Flush();
 
  private:
   std::string directory_;
   std::vector<std::string> lines_;
+  std::ofstream* file_;
+  std::time_t interval_;
+
+  std::time_t current_interval() const;
+  void new_log_file(const std::time_t& interval);
 };
 
 }
